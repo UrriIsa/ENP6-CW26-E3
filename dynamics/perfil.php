697f9f02036696cbdf10fb5fd8128dd1878018a1
Luisa Helena <323228558@alumno.enp.unam.mx>
2026-06-11 15:30:38 -0600

    style(perfil.css) añade archivo de estilo al perfil

--- dynamics/perfil.php
+++ dynamics/perfil.php
@@ -10,6 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../statics/css/perfil.css">
 
     <title>Perfil</title>
 </head>
