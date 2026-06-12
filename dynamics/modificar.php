b1a4d62e1895535c01d433611af2c21699467118
Erik Patricio Mora Santillán <325105660@alumno.enp.unam.mx>
2026-06-12 15:05:00 -0600

    feat(modificar.php): añade nuevos atributos ete, grupo, plantel y funcionalidad para modificar solamente uno o más atributos
    Co-authored-by: Abi <325273994@alumno.enp.unam.mx>

--- dynamics/modificar.php
+++ dynamics/modificar.php
@@ -6,19 +6,56 @@
         require './conexion.php';
         $con = connect();
         //Incluímos conexion.php y designamos connect a $con
-        $username = trim($_POST["username"]);
-        $password = trim($_POST["password"]);
-        $name = $_POST["name"];
+        $username = $_POST["username"];
         $new_username = $_POST["new_username"];
         $new_password = $_POST["new_password"];
         $new_name = $_POST["new_name"];
+        $new_ete = $_POST["new_ete"];
+        $new_plantel = $_POST["new_plantel"];
+        $new_grupo = $_POST["new_grupo"];
+        $mod_val = array();
+        $result = array();
         //Quitamos espacios
         try 
         {
-            $mod_val = "UPDATE alumnos SET nocta = '$new_username', contraseña = '$new_password', nombre = '$new_name' WHERE nombre = '$name' AND nocta = '$username' AND contraseña = '$password'";
-            $result = mysqli_query($con, $mod_val);
+            if(isset($new_password) && !empty($new_password)) 
+            {
+                $mod_val[] = "UPDATE alumnos SET contraseña = '$new_password' WHERE  nocta = '$username'";
+            }
+            if(isset($new_name) && !empty($new_name))
+            {
+                $mod_val[] = "UPDATE alumnos SET nombre = '$new_name' WHERE  nocta = '$username'";
+            }
+            if(isset($new_ete) && !empty($new_ete)) 
+            {
+                $mod_val[] = "UPDATE alumnos SET ete = '$new_ete' WHERE  nocta = '$username'";
+            }
+            if(isset($new_plantel) && !empty($new_plantel)) 
+            {
+                $mod_val[] = "UPDATE alumnos SET plantel = '$new_plantel' WHERE  nocta = '$username'";
+            }
+            if(isset($new_grupo) && !empty($new_grupo))
+            {
+                var_dump($new_grupo);
+                $mod_val[] = "UPDATE alumnos SET grupo = '$new_grupo' WHERE  nocta = '$username'";
+            }
+            if(isset($new_username) && !empty($new_username))
+            {
+                $mod_val[] = "UPDATE alumnos SET nocta = '$new_username' WHERE  nocta = '$username'";
+            }
+            if(!empty($mod_val))
+            {
+                foreach($mod_val as $mod) {
+                    $result[] = mysqli_query($con, $mod);
+                }
+                echo "<h1>Cambios realizados exitosamente</h1>";
+            } else
+            {
+                echo "<h1>No se realizó ningún cambio (no seleccionaste ninguna opción)</h1>";
+            }
         } catch(mysqli_sql_exception $e)
         {
+            var_dump($e);
             echo "<h1>Esa acción no se puede realizar</h1>";
         }
     }
