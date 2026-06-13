5a9e7f0f4b392b353b3a721d3a93865691c419c9
abigail zapata gonzalez <325273994@alumno.enp.unam.mx>
2026-06-13 11:03:28 -0600

    fix(prof-estadisticas.php): corregir ruta en nav

--- dynamics/prof-estadisticas.php
+++ dynamics/prof-estadisticas.php
@@ -37,7 +37,7 @@
                 Por el momento todos llevan a esta página-->
             <a id="boton-nav" href="./docente.php">Regresar</a>
             <a id="boton-nav" href="./prof-estadisticas.php">Datos estadísticos</a>
-            <a id="boton-nav" href="./desactivar_usuarios.php">Administrar usuarios</a>
+            <a id="boton-nav" href="./admin_general_usuarios.php">Administrar usuarios</a>
         </nav>
         <main>
             <h2>Datos estadísticos</h2>
