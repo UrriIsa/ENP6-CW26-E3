483818c4d72c5708bef7aae1614038764e03799d
Luisa Helena <323228558@alumno.enp.unam.mx>
2026-06-15 15:35:25 -0600

    style(perfil.php): agrega diseño a la seccion perfil

--- dynamics/perfil.php
+++ dynamics/perfil.php
@@ -2,7 +2,9 @@
     session_start();
     // Aquí va la validación del rol del usuario
     $usuario = $_SESSION["username"];
-    $rol = $_SESSION["rol"];
+    $name = $_SESSION["name"];
+    $grupo = $_SESSION["grupo"];
+    $plantel = $_SESSION["plantel"];
 
 ?>
 <!DOCTYPE html>
@@ -17,22 +19,38 @@
         <title>Perfil</title>
         <title>Perfil de Usuario</title>
     </head>
-
+    
     <body>
         <header> 
             <div id="barra-logos">
-            <img src= "../statics/imgs/logo-unam.png" class="logos" alt="logo UNAM">
-            <img src="../statics/imgs/logo-prepa6.png" class="logos" alt="logo prepa seis">
-            <img src="../statics/imgs/logo-ete.png" class="logos" alt="logo estudios técnicos especializados">
-            <a href="#"> Regresar </a>
-        </div>
-
+                <img src= "../statics/imgs/logo-unam.png" class="logos" alt="logo UNAM">
+                <img src="../statics/imgs/logo-prepa6.png" class="logos" alt="logo prepa seis">
+                <img src="../statics/imgs/logo-ete.png" class="logos" alt="logo estudios técnicos especializados">
+                <a href="../templates/inicio-alumno" id="regresar"> Regresar </a>
+            </div>
         </header>
+        <?php
+            echo "<h1> Bienvenid@ $name </h1>";
+        ?> <hr>
+        
         <div id="contenedor">
-                <?php
-                    echo "<h1>Hola $usuario.</h1>";
-                    echo "<p>Gracias por ser $rol del ETEC</p>";
-                ?>
+            <h2 class="info-usuario"> Nombre: </h2>
+            <?php
+            echo "<p> $name </h1>";
+            ?> 
+            <h2 class="info-usuario"> No. de cuenta: </h2>
+            <?php
+            echo "<p> $numero-cuenta </h1>";
+            ?> 
+            <h2 class="info-usuario"> Plantel: </h2>
+            <?php
+            echo "<p> $plantel </h1>";
+            ?> 
+            <h2 class="info-usuario"> Grupo ETEC: </h2>
+            <?php
+            echo "<p> $grupo </h1>";
+            ?> 
+
         </div>
     </body>
 </html>
