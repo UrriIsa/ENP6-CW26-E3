6579fccacac57fc6f6c834abde12eeeff6d596ef
Erik Patricio Mora Santillán <325105660@alumno.enp.unam.mx>
2026-06-16 11:34:17 -0600

    feat(veri.php): añade maquetado dependiente de si los datos se insertaron o no

--- dynamics/veri.php
+++ dynamics/veri.php
@@ -1,10 +1,9 @@
 <?php
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
     include 'conecta.php';
-    echo "aaaaa";
     if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST))
     {
         // $nombre = $_POST["nombre"];
-        echo "sirve";
 
 
                 // $conexion
@@ -23,13 +22,74 @@
         // mysql_escape_string($v)
         //
         $valores_cadena = implode(", ", $valores_escapados); 
-        $sql = "INSERT INTO formulario ($nombres_columnas) VALUES ($valores_cadena)" ;
-        var_dump($sql);
-        $resultado = mysqli_query($conexion, $sql);
-        if($resultado){
-            echo "Datos insertados correctamente" ;
-        }else{
-            echo " Error " ;
+        
+        try
+        {
+            $sql = "INSERT INTO formulario ($nombres_columnas) VALUES ($valores_cadena)" ;
+            $resultado = mysqli_query($conexion, $sql);
+?>
+            <!DOCTYPE html>
+            <html lang="es">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" href="../statics/css/form-diagnos.css">
+                    <title>Formulario Diagnóstico - MetETE a estudiar</title>
+                </head>
+                <body>
+                    <header>
+                        <!--agregue un div y un class-->
+                        <div class="head">
+                            <!--logo de la enp6 y unam y titulo del sitio-->
+                            <div class="head-left">
+                                <img src="../statics/imgs/logo-unam.png" alt="logo-unam" class="logo-unam" width="10%" height="10%">
+                                <img src="../statics/imgs/logo-enp6.png" alt="logo-enp6" class="logo-enp6" width="5%" height="5%">
+                                <h1>MetETE a estudiar</h1>
+                            </div>
+                            <!--Foto de perfil-->
+                            <img src="../statics/imgs/perfil.png" alt="foto-perfil" class="perfil" width="10%" height="10%">
+                        </div>
+                    </header>
+                    <main>
+                        <h2>¡Datos insertados correctamente!</h2>
+                        <h3><a href="../templates/inicio-alumno.html">Regresar</a></h3>
+                    </main>
+                </body>
+            </html>
+<?php
+        } catch(mysqli_sql_exception $e)
+        {            
+?>     
+            <!DOCTYPE html>
+            <html lang="es">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" href="../statics/css/form-diagnos.css">
+                    <title>Formulario Diagnóstico - MetETE a estudiar</title>
+                </head>
+                <body>
+                    <header>
+                        <!--agregue un div y un class-->
+                        <div class="head">
+                            <!--logo de la enp6 y unam y titulo del sitio-->
+                            <div class="head-left">
+                                <img src="../statics/imgs/logo-unam.png" alt="logo-unam" class="logo-unam" width="10%" height="10%">
+                                <img src="../statics/imgs/logo-enp6.png" alt="logo-enp6" class="logo-enp6" width="5%" height="5%">
+                                <h1>MetETE a estudiar</h1>
+                            </div>
+                            <!--Foto de perfil-->
+                            <img src="../statics/imgs/perfil.png" alt="foto-perfil" class="perfil" width="10%" height="10%">
+                        </div>
+                    </header>
+                    <main>
+                        <h2>Error al insertar los datos</h2>
+                        <h3><a href="../templates/inicio-alumno.html">Regresar</a></h3>
+                        <p>Verifica que hayas iniciado sesión e ingresado todo correctamente</p>
+                    </main>
+                </body>
+            </html>
+<?php
         }
         
     }
